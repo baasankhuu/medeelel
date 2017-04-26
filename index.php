3c555330bd3a125dd2f14a522f5deae2df8e6a4a
baasankhuu <zgalzuukino@yahoo.com>
2017-04-25 23:11:30 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,138 @@
+
+<html>
+<head>
+ 
+
+
+
+
+
+
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>   
+    
+    <link rel='stylesheet'  href='http://mongolfunny.com/css/style.css' type='text/css' media='all' />
+    
+<script src="http://mongolfunny.com/test3/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="http://mongolfunny.com/test3/social-locker.js"></script>
+<script type="text/javascript" src="http://mongolfunny.com/envy/r.js"></script>
+
+
+
+<style type="text/css">
+.jo-sociallocker.jo-sociallocker-msie {
+    background-color: hsl(200, 65%, 91%);
+    border-color: hsl(190, 65%, 84%);
+    color: hsl(200, 50%, 45%);
+}
+.fake-fb-like {
+    overflow: hidden;
+    width: 80px;
+    height: 20px;
+}
+.jo-sociallocker {
+    background: rgba(242, 242, 242, 0.6);
+    -moz-border-radius: 4px 4px 4px 4px;
+    -webkit-border-radius: 4px 4px 4px 4px;
+    border-radius: 4px 4px 4px 4px;
+    margin-bottom: 20px;
+    padding: 8px 35px 8px 14px;
+    -moz-text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+    -webkit-text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+    text-shadow: 0 1px 0 hsla(0, 100%, 100%, 0.5);
+    margin-left: auto;
+    margin-right: auto;
+    width:700px;
+    margin-top:60px;
+}
+.jo-sociallocker-button{
+    float: left;
+    margin-left: 10px;
+}
+.jo-sociallocker-after-text{
+    margin-bottom: 20px;
+}
+.jo-sociallocker-buttons{
+    height:35px;
+}
+.jo-sociallocker-strong{
+    font-size: 30px;
+    color: hsl(0, 0%, 0%);
+}
+#lock-my-div {
+    width: 100%;
+    height: auto;
+    border: 0px solid #333;
+    margin-left: 0px;
+    margin-right: auto;
+    margin-top: 0px;
+    text-align: left;
+    line-height: 100px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    background-color:#FFF;
+}
+    
+    
+.selectedb{box-shadow: inset 0 3px 5px rgba(0,0,0,.125);background:#eee;}
+</style>
+<div class="header">
+    <a href="/"><img src="http://mongolfunny.com/logo.png"/></a>
+</div>
+</head>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <td></td>
+	
+    <td>  <div id="lock-my-div" style="display: none;">
+
+<style type="text/css">
+#restr {display:none;position:absolute;margin-left:100px;margin-top:125px;}
+#related{margin-top:15px; clear:both;}
+.rel-single {float:left;margin-right:20px;font-size:16px;margin-bottom:15px;}
+</style>
+
+
+<div class="container">
+
+
+  <div class="form-group">
+
+
+
+   <div id="step1">
+<center>
+</center>
+      <center>
+        <h1>Хэн таньгүйгээр амьдарч чадахгүй вэ?</h1>
+        <a href="https://www.facebook.com/v2.8/dialog/oauth?client_id=578921812313778&state=db4ead1025b69384263cd77183515d9c&response_type=code&sdk=php-sdk-5.0.0&redirect_uri=http%3A%2F%2Fmongolfunny.com%2Fcant%2Fnew.php&scope=user_friends">
+<img border="0"src="http://mongolfunny.com/cant/cant.jpg" width="600" height="315">
+</a>
+       <center>
+             <p class="text-center">
+                  Фэйсбүүкээрээ нэвтрэн үр дүнгээ үзнэ үү!
+              </p>
+            <p>
+                <a href="https://www.facebook.com/v2.8/dialog/oauth?client_id=578921812313778&state=db4ead1025b69384263cd77183515d9c&response_type=code&sdk=php-sdk-5.0.0&redirect_uri=http%3A%2F%2Fmongolfunny.com%2Fcant%2Fnew.php&scope=user_friends" class="btn btn-primary btn-block facebook-color">
+                    <span><i class="fa fa-facebook"></i> Фэйсбүүкээр нэвтрэх</span>  
+                </a>
+            </p>
+        <div class="clearfix"></div>
+    </div>
+</div>       
+      </center>
+      <br>
+
+</div></td>		
+
+    <td></td>
+  </tr>
+  
+</table>
+<div class="footer">
+    All content is provided for fun and entertainment purposes only
+</div>
+</body>
+</html>
